125d2562cad204da1eb0c5d266e67e203443c245
AHILYND <AHILYND@UDEL.EDU>
2021-09-22 17:28:13 -0400

    naming tabs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+import ChangeLog from './pages/change log';
+import Contact from './pages/contact';
+import Movies from './pages/movies';
+import Reflections from './pages/reflections';
+//import Resources from './pages/resources';
+import ToDoLists from './pages/todo list';
 
 function App() {
   return (
@@ -12,6 +22,18 @@ function App() {
       <p>Pronouns: she/her/hers</p>
       <p> Contact Info: ahilynd@udel.edu</p>
       <p>New Main Repository?</p>
+      <Router>
+      <Navbar />
+      <Switch>
+         <Route path="/" exact component={Home} />
+        < Route path='/about' component={About} />
+        <Route path='/change log' component={ChangeLog} />
+        <Route path='/contact' component={Contact} />
+        <Route path='/movies' component={Movies} />
+        <Route path='/todo list' component={ToDoLists} />
+        <Route path='/reflections' component={Reflections} />
+      </Switch>
+      </Router>
 
     </div>
     
