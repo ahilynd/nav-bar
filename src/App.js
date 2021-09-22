724fa36af601754c08d3d33b84fd347a0cc8738d
AHILYND <AHILYND@UDEL.EDU>
2021-09-22 13:26:28 -0400

    might make this my main rep

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ function App() {
   return (
     <div className="App">
       <h1>By: Ahilyn Dipre</h1>
-      <h2>Hello WOrld </h2>
+      <h2>Hello World </h2>
+      <h1>Welcome to Ahilyn's page!</h1>
+      <p>Name: Ahilyn Dipre</p>
+      <p>Major: Computer Science BS</p>
+      <p>Pronouns: she/her/hers</p>
+      <p> Contact Info: ahilynd@udel.edu</p>
+      
     </div>
   );
 }
