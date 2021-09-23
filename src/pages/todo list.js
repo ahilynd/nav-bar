c62456429e1d20b8b1f20f16a74cdd7486e38bbd
AHILYND <AHILYND@UDEL.EDU>
2021-09-22 23:11:35 -0400

    updating todo list

--- src/pages/todo list.js
+++ src/pages/todo list.js
@@ -4,13 +4,37 @@ const ToDoLists = () => {
     return (
         <div
         style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'Right',
+            display: '',
+            justifyContent: 'Left',
+            alignItems: 'center',
             height: '100vh'
         }}
         >
-            <h1>Plans For Upcoming Milestones</h1>
+        <h2>TODO:</h2>
+          <ul>
+                 <li>I want to start working on my layout and seperate my different sections of the page.</li>
+                 <li>There's an idea of a theme that I have in mind that I'm looking to start implementing them by next week.</li>
+                 <li>Want to make it so that the corresponding information can be accessed when someone clicks on my nagivation bar.</li>
+                  <a href="https://www.youtube.com/watch?v=gXkqy0b4M5g">Tutorial I Want to Follow for Navigation Bar</a>
+                 <li>I watch a lot of movies and enjoy doing that so I want to have a special place for my opinions on movies
+                      or something like that somewhere on my site!</li>
+                 <li>I would also like to start this tutorial.</li> <a href="https://reactjs.org/tutorial/tutorial.html">Tic Tac Toe</a>
+          </ul>
+          <h2>TODO 2.0:</h2>
+          <ul>
+             <li>Basically the same as last time since I didn't go through them this week!</li>
+             <li>I would also like to start focusing much more on REACT and Typescript which I believe I will get the 
+                chance to do once I start working on the Navigation Bar and Tic Tac Toe game.
+             </li>
+             <li>I'm going to rewatch Dr.Bart's lecture from today (9/16/21) and try to implement the tricks he showed us for my own ideas.</li>
+             <li>Fix email part up above to include an enter button.</li>
+          </ul>
+          <h2>TODO 3.0:</h2>
+          <ul>
+              <li>Mess around with the format of my page and fix things around like the style/other CSS properties.</li>
+              <li>Implement a scratch off game under Movies tab.</li>
+              <li>Try to make my page more mobile friendly</li>
+          </ul>
         </div>
     );
 };
