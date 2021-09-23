f99f5dfc23110ee68830ecf833bbd7f4ba9d7194
AHILYND <AHILYND@UDEL.EDU>
2021-09-22 20:21:12 -0400

    adding links under resources tab

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -10,7 +10,19 @@ const Resources = () => {
             height: '100vh'
         }}
         >
-            <h1>Tutorials That Have Helped Me Create My Site</h1>
+        <body>
+        <p><a href="https://www.tutorialrepublic.com/html-tutorial/html-links.php">Tutorial for Links</a></p>
+        <p><a href="https://www.tutorialrepublic.com/html-tutorial/html-paragraphs.php">Tutorial for Paragraphs</a></p>
+        <p><a href="https://www.youtube.com/watch?v=BA_c3bGQXlQ">Setting Up GitHub Page</a></p>
+        <p><a href="https://www.w3schools.com/html/html_images.asp">Adding Images</a></p>
+        <p><a href="https://udel.instructure.com/courses/1601987/external_tools/151642">Dr.Bart's Tutorial</a></p>
+        <p><a href="https://www.youtube.com/watch?v=VzWBLj_CfpE">Create NavBar</a></p>
+        <p><a href="https://www.atlassian.com/git/tutorials/using-branches/merge-conflicts">Merging</a></p>
+        <p><a href="https://support.atlassian.com/bitbucket-cloud/docs/configure-your-dvcs-username-for-commits/">Connect Username</a></p>
+        <p><a href="https://www.youtube.com/watch?v=F8s4Ng-re0E">Transfer React to Github</a></p>
+        <p><a href="https://www.youtube.com/watch?v=GTj-s8hGaUM">Deploy React to GitHub</a></p>
+
+        </body> 
         </div>
     );
 };
